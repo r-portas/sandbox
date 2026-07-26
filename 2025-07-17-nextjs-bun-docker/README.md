65b56ff91de99fabb99b6b9f34b0e0142bebad78
Roy Portas <royportas@gmail.com>
2026-07-27 09:01:56 +1000

    Remove old stuff, start fresh

--- 2025-07-17-nextjs-bun-docker/README.md
+++
@@ -1,9 +0,0 @@
-# Next.js with Bun and Docker
-
-An example of how to run a Next.js application using Bun and Docker.
-
-## Running
-
-```bash
-docker compose up
-```
--- 2025-07-17-nextjs-bun-docker/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // Build as a standalone application
-  output: "standalone",
-};
-
-export default nextConfig;
